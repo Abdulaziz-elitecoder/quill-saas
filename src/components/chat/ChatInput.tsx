c015237e1a156e62a3ae71558e6cd205a6bc23c1
Abdulaziz-elitecoder <abdulaziz.muhammed.elnoti@gmail.com>
2024-02-21 23:56:58 +0200

    create boilerplate components for the ChatWrapper part of the screen

---
+++ src/components/chat/ChatInput.tsx
@@ -0,0 +1,9 @@
+const ChatInput = () => {
+
+    return (<div>
+        chatInput
+    </div>)
+
+}
+
+export default ChatInput
---
+++ src/components/chat/ChatWrapper.tsx
@@ -0,0 +1,27 @@
+import { trpc } from "@/app/_trpc/client";
+import ChatInput from "./ChatInput";
+import Messages from "./Messages";
+
+
+interface ChatWrapperProps {
+  fileId: string
+}
+
+const ChatWrapper = ({fileId}: ChatWrapperProps) => {
+
+  const {} = trpc.getFileUPloadStatus.useQuery({
+    fileId
+  })
+
+  return (
+    <div className="relative min-h-full bg-zinc-50 flex divide-y divide-zinc-200 flex-col justify-between gap-2">
+      <div className="flex-1  justify-between flex flex-col mb-28">
+        <Messages />
+      </div>
+
+      <ChatInput />
+    </div>
+  );
+};
+
+export default ChatWrapper;
